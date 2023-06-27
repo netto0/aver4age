df1cbfb47425f1a326b292f542b2cbb3d81aa713
Netto <orlandiomnetto@gmail.com>
2023-06-27 10:38:30 -0300

    repository creation and header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './Header.module.css'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>AVER<span>4</span>AGE</h1>
+            <a href="">INÍCIO</a>
+            <a href="">MÉDIAS</a>
+            <a href="">CONFIGURAÇÕES</a>
+            <a href="">PERFIL</a>
+        </header>
+    )
+}
